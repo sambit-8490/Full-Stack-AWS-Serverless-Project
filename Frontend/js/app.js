754afd7e9bba722e6bab70999f12c5e2d4032126
sambit-8490 <Sambitkumarchaudhury786@gmail.com>
2026-09-18 11:09:12 +0530

    Update app.js

--- Frontend/js/app.js
+++ Frontend/js/app.js
@@ -2,7 +2,7 @@
  * FETCH STUDENT DATA (unchanged API logic)
  ********************************************/
 var fetchStudentData = () => {
-    var endpoint = 'https://vmgjhpngo6.execute-api.us-east-1.amazonaws.com/student-api/getStudent';
+    var endpoint = 'https://4jf1jam416.execute-api.us-east-1.amazonaws.com/student-api/get-student';
 
     return fetch(endpoint)
         .then(response => {
@@ -53,7 +53,7 @@ var addStudent = (rollNumber, studentName, className) => {
 
     closeModal(); // hide modal before submitting
 
-    var endpoint = 'https://vmgjhpngo6.execute-api.us-east-1.amazonaws.com/student-api/addStudent';
+    var endpoint = 'https://4jf1jam416.execute-api.us-east-1.amazonaws.com/student-api/add-student';
 
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
